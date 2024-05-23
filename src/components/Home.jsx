10dd4f517edab72b745cf004e7bf79b97d67d317
priyanshu <priyanshu24pandey@gmail.com>
2024-05-23 18:04:33 +0530

    added tailwind and styled home pgae

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 
+import { Link } from "react-router-dom";
 import Modal from "./Modal";
 import dayjs from "dayjs";
 import musicFile from "./../assets/track.mp3";
@@ -66,18 +67,41 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>Milking Tracker with Music</h1>
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-full flex justify-between items-center mb-6 p-4 bg-blue-200">
+        <h1 className="text-3xl font-bold">Milking Tracker</h1>
+        <Link
+          to="/history"
+          className="text-green-700 hover:underline font-semibold"
+        >
+          History
+        </Link>
+      </div>
       {milking ? (
         <>
-          <button onClick={paused ? handleResume : handlePause}>
-            {paused ? "Resume" : "Pause"}
-          </button>
-          <button onClick={stopMilking}>Stop Milking</button>
-          <div>{timer} seconds</div>
+          <div className="flex flex-col items-center gap-5">
+            <button
+              onClick={paused ? handleResume : handlePause}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              {paused ? "Resume" : "Pause"}
+            </button>
+            <button
+              onClick={stopMilking}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Stop Milking
+            </button>
+            <div className="text-xl">{timer} seconds</div>
+          </div>
         </>
       ) : (
-        <button onClick={startMilking}>Start Milking</button>
+        <button
+          onClick={startMilking}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Start Milking
+        </button>
       )}
       <audio ref={audioRef} loop />
 
